4731d161a29f212e7f1cf6d0f22cba490aa4c9c4
hyunji <phj9573@naver.com>
2024-03-22 12:34:43 +0900

    first commit

---
+++ src/main/java/com/example/tripbridgeserver/TripbridgeServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.tripbridgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripbridgeServerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(TripbridgeServerApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/tripbridgeserver/TripbridgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripbridgeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripbridgeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
