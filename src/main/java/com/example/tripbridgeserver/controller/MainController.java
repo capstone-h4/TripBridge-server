45c7ff2cb7accc7079a731ccb99c4f04c4e2e1e6
hyunji <phj9573@naver.com>
2024-03-25 14:43:44 +0900

    fix : change mainController

--- src/main/java/com/example/tripbridgeserver/controller/MainController.java
+++ src/main/java/com/example/tripbridgeserver/controller/MainController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class hello {
+public class MainController {
     @GetMapping("/")
-    public String test() {
+    public String home() {
         return "Hello World!";
     }
 
