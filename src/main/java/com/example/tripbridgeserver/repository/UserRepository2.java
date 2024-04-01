71ebecf7ac0f892da5ded0b4f2f4a2f58f92204e
yoon hyeon soo <chris24da@naver.com>
2024-04-01 14:34:07 +0900

    chore: UserRepository 이름 수정

--- src/main/java/com/example/tripbridgeserver/repository/UserRepository2.java
+++ src/main/java/com/example/tripbridgeserver/repository/UserRepository2.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository1 extends JpaRepository<User,Long> {
+public interface UserRepository2 extends JpaRepository<User,Long> {
 }
