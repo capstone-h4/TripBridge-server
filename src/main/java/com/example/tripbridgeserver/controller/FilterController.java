564954e427b90739ecb3a3376ef05455419d69b7
hyunji <phj9573@naver.com>
2024-04-28 22:44:32 +0900

    feat: 공공 api 호출 파라미터 수정

--- src/main/java/com/example/tripbridgeserver/controller/FilterController.java
+++ src/main/java/com/example/tripbridgeserver/controller/FilterController.java
@@ -1,6 +1,7 @@
 package com.example.tripbridgeserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
@@ -13,17 +14,28 @@ public class FilterController {
 
     // 공공 api 이용한 장소 필터링 기능
     // encoding 서비스키 사용
-    @GetMapping("/place")
-    public String showPlace(){
+    @GetMapping("/place/list")
+    public String showPlace(
+          @RequestParam("areaCode") String areaCode,
+          @RequestParam("contentTypeId") String contentTypeId,
+          @RequestParam("cat1") String cat1,
+          @RequestParam("cat2") String cat2
+//            @RequestParam("cat3") String cat3,
+
+    ){
+
         StringBuffer result = new StringBuffer();
         try{
             String urlstr = "https://apis.data.go.kr/B551011/KorService1/areaBasedList1?" +
                     "numOfRows=30" +
                     "&MobileOS=ETC" +
                     "&MobileApp=TestApp" +
-                    "&contentTypeId=12" +
-                    "&areaCode=1&cat1=A01&" +
-                    "serviceKey=YGF0y1fe6pkkysTM1WzMH9htWqLp7iiY2fuy%2BJiEFoI%2BKH%2BON1EnErgGxvE6T2Z5awLIIAlQcuD9hjylPI6GYg%3D%3D" +
+                    "&areaCode=" + areaCode +
+                    "&contentTypeId=" + contentTypeId +
+                    "&cat1=" + cat1 +
+                    "&cat2=" + cat2 +
+//                    "&cat3=" + cat3 +
+                    "&serviceKey=YGF0y1fe6pkkysTM1WzMH9htWqLp7iiY2fuy%2BJiEFoI%2BKH%2BON1EnErgGxvE6T2Z5awLIIAlQcuD9hjylPI6GYg%3D%3D" +
                     "&_type=json";
             URL url = new URL(urlstr);
 
@@ -41,9 +53,13 @@ public class FilterController {
 
         } catch (Exception e) {
             e.printStackTrace();
+
         }
+
         return result+"</xmp>";
     }
+
+
 
 
 
