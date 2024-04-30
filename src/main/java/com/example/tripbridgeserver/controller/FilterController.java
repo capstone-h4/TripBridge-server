b882f039f58b5c70dac72376082822ea561acbdc
hyunji <phj9573@naver.com>
2024-04-30 12:29:13 +0900

    수정

--- src/main/java/com/example/tripbridgeserver/controller/FilterController.java
+++ src/main/java/com/example/tripbridgeserver/controller/FilterController.java
@@ -12,17 +12,30 @@ import java.net.URL;
 @RestController
 public class FilterController {
 
+    //서비스,dto 등 분리하기
+
     // 공공 api 이용한 장소 필터링 기능
     // encoding 서비스키 사용
     @GetMapping("/place/list")
     public String showPlace(
-          @RequestParam("areaCode") String areaCode,
-          @RequestParam("contentTypeId") String contentTypeId,
-          @RequestParam("cat1") String cat1,
-          @RequestParam("cat2") String cat2
+//          @RequestParam("areaCode") String areaCode,
+//          @RequestParam("contentTypeId") String contentTypeId,
+//          @RequestParam("cat1") String cat1,
+//          @RequestParam("cat2") String cat2
 //            @RequestParam("cat3") String cat3,
+          @RequestParam(value = "areaCode", required = false, defaultValue = "") String areaCode,
+          @RequestParam(value = "contentTypeId", required = false, defaultValue = "") String contentTypeId,
+          @RequestParam(value = "cat1", required = false, defaultValue = "") String cat1,
+          @RequestParam(value = "cat2", required = false, defaultValue = "") String cat2
 
-    ){
+          ){
+
+        // null을 체크하고, null일 경우 빈 문자열로 대체
+        areaCode = (areaCode != null) ? areaCode : "";
+        contentTypeId = (contentTypeId != null) ? contentTypeId : "";
+        cat1 = (cat1 != null) ? cat1 : "";
+        cat2 = (cat2 != null) ? cat2 : "";
+        //cat3 = (cat3 != null) ? cat3 : "";
 
         StringBuffer result = new StringBuffer();
         try{
