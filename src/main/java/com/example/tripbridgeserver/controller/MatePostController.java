34f65917f5fb65fcd69276be004e9af5e85ea28f
yoon hyeon soo <chris24da@naver.com>
2024-05-09 22:02:32 +0900

    직전 수정사항 삭제

--- src/main/java/com/example/tripbridgeserver/controller/MatePostController.java
+++ src/main/java/com/example/tripbridgeserver/controller/MatePostController.java
@@ -9,7 +9,6 @@ import com.example.tripbridgeserver.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -82,7 +81,6 @@ public class MatePostController {
     }
 
     @DeleteMapping("/mate/{id}")
-    @PreAuthorize("@securityService.isCurrentUser(#id)")
     public ResponseEntity<MatePost> delete(@PathVariable Long id){
         MatePost target = matePostRepository.findById(id).orElse(null);
         if(target==null){
