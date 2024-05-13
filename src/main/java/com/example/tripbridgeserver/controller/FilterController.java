0c1118270cd427c3ea6227efb121747266b0fb18
hyunji <phj9573@naver.com>
2024-05-14 01:35:47 +0900

    feat: 장소상세정보 구현

--- src/main/java/com/example/tripbridgeserver/controller/FilterController.java
+++ src/main/java/com/example/tripbridgeserver/controller/FilterController.java
@@ -33,11 +33,21 @@ public class FilterController {
         } catch (FilterServiceException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("데이터를 가져오는 중 오류가 발생하였습니다.");
         }
-
     }
 
+    // 장소 상세정보 조회
+    @GetMapping("/place")
+    public ResponseEntity<String> showPlaceInfo(
+            @RequestParam("contentTypeId") String contentTypeId,
+            @RequestParam("contentId") String contentId
+    ){
+        try {
+            String result = filterService.getDeatiledPlaceInfo(contentTypeId, contentId);
+            return ResponseEntity.ok(result);
+        } catch (FilterServiceException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("데이터를 가져오는 중 오류가 발생하였습니다.");
+        }
 
 
-
-
+    }
 }
