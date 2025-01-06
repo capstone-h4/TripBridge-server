31d706b6e380e9b9329840152aabecc6dc63a838
hyunji <phj9573@naver.com>
2025-01-06 12:54:17 +0900

    feat: 불필요한 코드 정리(#58)

--- src/main/java/com/example/tripbridgeserver/common/JwtProvider.java
+++ src/main/java/com/example/tripbridgeserver/common/JwtProvider.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
