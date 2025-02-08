a190e15af699553881a7bfe6901b68b606cfec6c
hyunji <phj9573@naver.com>
2025-02-08 20:21:22 +0900

    fix : 누락된 어노테이션 추가(#66)

--- src/main/java/com/example/tripbridgeserver/service/ChatBotService.java
+++ src/main/java/com/example/tripbridgeserver/service/ChatBotService.java
@@ -2,6 +2,7 @@ package com.example.tripbridgeserver.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -31,6 +32,8 @@ public class ChatBotService {
 
 	private final UserRepository userRepository;
 	private final ChatRouteRepository chatRouteRepository;
+
+	@Autowired
 	private RestTemplate restTemplate;
 
 	public String generateNearPlaces(String choicePlace) {
